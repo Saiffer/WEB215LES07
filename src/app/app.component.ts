6b19f5e80e928ee3cb16d4afbbb6fd87e26dfa05
Saiffer <32097584+Saiffer@users.noreply.github.com>
2020-06-12 15:04:47 -0400

    angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { falsService } from '../app/example/exam.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [falsService]
 })
 export class AppComponent {
-  title = 'my-first-app';
+  title= '';
+  constructor(private fals: falsService) {}
+  funcDemo(qty) {
+    this.fals.falsPoster(qty);
+  }
 }
